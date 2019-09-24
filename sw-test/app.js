bd6123495315c458804f328846a81b98806f9bfd
zhucuicui <zhucuicui@jd.com>
2019-09-24 15:46:54 +0800

    add

---
+++ sw-test/app.js
@@ -0,0 +1,119 @@
+// register service worker
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    
+    if (window.PushManager) {
+      reg.pushManager.getSubscription().then(subscription => {
+          // 如果用户没有订阅
+          if (!subscription) {
+              subscribeUser(reg);
+          } else {
+              console.log("You have subscribed our notification");
+          }       
+      }); 
+    }    
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
+// function for loading each image via XHR
+
+function imgLoad(imgJSON) {
+  // return a promise for an image loading
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', imgJSON.url);
+    request.responseType = 'blob';
+
+    request.onload = function() {
+      if (request.status == 200) {
+        var arrayResponse = [];
+        arrayResponse[0] = request.response;
+        arrayResponse[1] = imgJSON;
+        resolve(arrayResponse);
+      } else {
+        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+      }
+    };
+
+    request.onerror = function() {
+      reject(Error('There was a network error.'));
+    };
+
+    // Send the request
+    request.send();
+  });
+}
+
+var imgSection = document.querySelector('section');
+
+window.onload = function() {
+
+  // load each set of image, alt text, name and caption
+  for(var i = 0; i<=Gallery.images.length-1; i++) {
+    imgLoad(Gallery.images[i]).then(function(arrayResponse) {
+
+      var myImage = document.createElement('img');
+      var myFigure = document.createElement('figure');
+      var myCaption = document.createElement('caption');
+      var imageURL = window.URL.createObjectURL(arrayResponse[0]);
+
+      myImage.src = imageURL;
+      myImage.setAttribute('alt', arrayResponse[1].alt);
+      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
+
+      imgSection.appendChild(myFigure);
+      myFigure.appendChild(myImage);
+      myFigure.appendChild(myCaption);
+
+    }, function(Error) {
+      console.log(Error);
+    });
+  }
+};
+
+
+function subscribeUser(swRegistration) {
+  const applicationServerPublicKey = "BBlY_5OeDkp2zl_Hx9jFxymKyK4kQKZdzoCoe0L5RqpiV2eK0t4zx-d3JPHlISZ0P1nQdSZsxuA5SRlDB0MZWLw";
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+  })
+  // 用户同意
+  .then(function(subscription) {
+      console.log('User is subscribed:', JSON.stringify(subscription));
+       
+  })
+  // 用户不同意或者生成失败
+  .catch(function(err) {
+      console.log('Failed to subscribe the user: ', err);
+  });
+}
+
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
